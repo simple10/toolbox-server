5ca4ab40196c9462561c902d74b2e1bac790ff6a
Joe Johnston <joe@connect.me>
2013-05-09 18:11:37 -0700

    Remove path from utils

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
   , webshot = require('webshot')
   , utils = require('./lib/utils')
+  , join = require('path').join
   , webshot = require('./lib/webshot')
   , app = express()
   , tmpdir = '/tmp'
@@ -17,9 +18,13 @@ var getWebshot = function(url, file, cb) {
       width: 'window',
       height: 480
     },
+    // timeout after 25 seconds
+    timeout: 25000,
     script: function() {
       // todo: return page title in json response
-      console.log("Page Title: " + document.title);
+      //console.log("Page Title: " + document.title);
+
+      return "title: " + document.title;
     },
     userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us)'
       + ' AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
@@ -30,7 +35,7 @@ var getWebshot = function(url, file, cb) {
 app.get('/webshot/:url(*)', function(req, res){
   var url = utils.url(req.params.url)
     , id = utils.md5(url)
-    , file = utils.path.join(tmpdir, id + '.png');
+    , file = join(tmpdir, id + '.png');
   getWebshot(url, file, function(err){
     if (err) return console.log(err);
     console.log('OK');
